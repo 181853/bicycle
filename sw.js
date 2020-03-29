370f05a8195e51e75ad7107118454af459d2a237
Mateusz Szot <s181853@student.uek.krakow.pl>
2020-03-29 11:50:48 +0200

    update sw

--- sw.js
+++ sw.js
@@ -4,11 +4,23 @@ const cacheName = 'v1';
 const cacheFiles = [
     './',
     './index.html',
-    './favicon.ico',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Pinarello_Dogma_XC_Shimano_XTR_Custom_Bike_%2816165258647%29.jpg/482px-Pinarello_Dogma_XC_Shimano_XTR_Custom_Bike_%2816165258647%29.jpg',
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Wiemu24.jpg/1280px-Wiemu24.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/3/3d/Bike_refelector_safety_flash.JPG",
+    "https://use.fontawesome.com/releases/v5.8.2/css/all.css",
     "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.1/css/mdb.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"
+    "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js",
+    './favicon.ico',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', function(e) {
